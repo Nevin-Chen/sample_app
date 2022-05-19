1fa8d7f1a6256535567cfd87556cb384c29415cf
Nevin-Chen <nevinchen6@gmail.com>
2022-05-19 20:16:13 +0000

    finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -14,19 +14,19 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
 
   test "should get help" do
-    get static_pages_help_url
+    get help_url
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
   end
   
   test "should get about" do
-    get static_pages_about_url
+    get about_url
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
   
   test "should get contact page" do
-    get static_pages_contact_url
+    get contact_url
     assert_response :success
     assert_select "title", "Contact | #{@base_title}"
   end
